23641e42b9e68aba5984853c96aa1efa02cf7ad5
Connor Keenum <nellakprime@gmail.com>
2025-09-07 19:27:35 -0500

    Fix the embed issue by altering the next config.

--- next.config.js
+++ next.config.js
@@ -3,12 +3,14 @@ const nextConfig = { // NOTE: we use next@15.1.1-canary.24 due to internal error
   reactStrictMode: true, // TODO: Figure out if it works with react strict mode on or not
   images: { minimumCacheTTL: 60, }, // https://www.codemotion.com/magazine/frontend/optimize-next-js-for-production/
   //compiler: { removeConsole: { exclude: ["error"],},}, // Issue with Next.js 15: https://github.com/vercel/next.js/issues/71974
-  async headers() {
-    return [{ source: "/(.*)", headers: [ 
-      { key: "Cross-Origin-Opener-Policy", value: "same-origin", }, 
-      { key: "Cross-Origin-Embedder-Policy", value: "require-corp",},
-      { key: "Content-Security-Policy", value: "frame-src 'self' https://www.youtube.com https://www.youtube-nocookie.com;"},
-    ],},]
-  }, // This eliminates the Google Sign-in / Sign-up error where it says the Cross Origin is disallowed. See also: https://github.com/vercel/next.js/discussions/51135
+  // async headers() {
+  //   return [{
+  //     source: "/(.*)",
+  //     // headers: [
+  //     //   { key: "Cross-Origin-Opener-Policy", value: "same-origin", },
+  //     //   { key: "Cross-Origin-Embedder-Policy", value: "require-corp", }, // Must be disabled to allow Iframes
+  //     // ],
+  //   },]
+  // }, // This eliminates the Google Sign-in / Sign-up error where it says the Cross Origin is disallowed. See also: https://github.com/vercel/next.js/discussions/51135
 }
 module.exports = nextConfig
--- src/UI/organisms/CardGrid/CardGrid.js
+++ src/UI/organisms/CardGrid/CardGrid.js
@@ -8,7 +8,7 @@ const cards = Array.from({ length: 4 }, (_, i) => `Card ${i + 1}`)
 const cols = { xs: 1, sm: 2, md: 4, lg: 5, xl: 6 }
 export const CardGrid = () => {
     return (
-        <Box sx={{ m: 4, mt: 2, p: 2, overflowY: 'auto', display: 'flex', flexDirection: 'column', gap: 3 }}>
+        <Box sx={{ m: 4, mt: 2, p: 2, height: '100%', overflowY: 'auto', display: 'flex', flexDirection: 'column', gap: 3 }}>
             <Typography variant='h1' component='h1' sx={{ fontSize: theme => theme.typography.h2.fontSize }}>Corporate Banner</Typography>
             <Grid container spacing={2} columns={cols}>
                 {cards.map((card) => (
@@ -22,29 +22,24 @@ export const CardGrid = () => {
                 ))}
             </Grid>
             <Typography variant='h1' component='h1' sx={{ fontSize: theme => theme.typography.h2.fontSize }}>Another Header</Typography>
-            <Grid container spacing={2} columns={cols}>
+            <Grid container spacing={2} columns={cols} sx={{ height: '100%' }}>
                 <Grid key={cards.length} size={cols}>
-                    <Card sx={{ height: '30vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <Card sx={{ height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                         <CardContent sx={{
                             padding: '0!important',
                             width: '100%',
                             height: '100%',
-                            '& iframe': { border: '0' }
                         }}>
-                            {/* <ReactPlayer
-                                src={`https://www.youtube.com/watch?v=mPaCnwpFvZY`}
+                            <ReactPlayer
+                                src='https://www.youtube-nocookie.com/watch?v=mPaCnwpFvZY'
                                 width='100%'
                                 height='100%'
-                                controls
                                 config={{
                                     youtube: {
-                                        playerVars: {
-                                            origin: 'https://www.youtube.com/',
-                                        },
+                                        playerVars: { modestbranding: 1, rel: 0 },
                                     },
                                 }}
-                            /> */}
-                            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/mPaCnwpFvZY" title="TanStack Query - How to become a React Query God" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                            />
                         </CardContent>
                     </Card>
                 </Grid>
