8158eba5482bd078ad274783bafedd3926f35268
Connor Keenum <nellakprime@proton.me>
2025-11-09 19:16:50 -0600

    Fix overlap of upper and lower Card Grid problem for mobile devices.

--- src/UI/molecules/CardGrid/CardGrid.elements.js
+++ src/UI/molecules/CardGrid/CardGrid.elements.js
@@ -1,5 +1,5 @@
 import { Box, styled } from '@mui/material'
 export const StyledCardGrid = styled(Box)(({ theme }) => ({
     margin: theme.spacing(4), marginTop: theme.spacing(2), padding: theme.spacing(2),
-    height: '100%', display: 'flex', flexDirection: 'column', gap: theme.spacing(3),
+    display: 'flex', flexDirection: 'column', gap: theme.spacing(3),
 }))
--- src/UI/templates/Home/HomeTemplate.js
+++ src/UI/templates/Home/HomeTemplate.js
@@ -3,10 +3,11 @@ import { SideNav } from '../../organisms/SideNav/SideNav.js'
 import { Root, MainContent } from './HomeTemplate.elements.js'
 import dynamic from 'next/dynamic.js'
 import { CardGridUpper } from './CardGrid/CardGridUpper/CardGridUpper.js'
+import { Box } from '@mui/material'
 const CardGridLower = dynamic(() => import('./CardGrid/CardGridLower/CardGridLower.js'), { ssr: false })
 export const HomeTemplate = () => (
   <Root>
     <SideNav />
-    <MainContent><Nav /><CardGridUpper /><CardGridLower /></MainContent>
+    <MainContent><Nav /><Box><CardGridUpper /><CardGridLower /></Box></MainContent>
   </Root>)
 export default HomeTemplate
